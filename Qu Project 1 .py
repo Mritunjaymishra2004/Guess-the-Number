6d907d4c317810c6ca3fd0f5f7e4449f6f587640
Mritunjay Mishra <mritunjaymishra7607197286@gmail.com>
2024-09-20 00:29:24 +0530

    Add files via upload

---
+++ Qu Project 1 .py
@@ -0,0 +1,17 @@
+# Guess the Number
+import random
+
+target = random.randint(1, 100)
+
+while True:
+    userChoice = int(input("Guess the target : "))
+    if(userChoice == target):
+        print("Success : Correct Guess!!")
+        break
+    elif(userChoice < target):
+        print("Your number was too small. Take a bigger guess..")
+    else:
+        print("Your number was too big. Take a smaller guess..")
+
+
+print("____GAME OVER____")
